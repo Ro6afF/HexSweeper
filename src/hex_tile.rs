506590f1a731120df91acbebfb290e9eb029d768
Ro6afF <dimo_chanev@windowslive.com>
2022-02-03 12:13:49 +0200

    check for click inside tile

--- src/hex_tile.rs
+++ src/hex_tile.rs
@@ -10,11 +10,44 @@ use std::f32::consts::PI;
 pub struct HexTile {
     pub size: f32,
     pub pos: Vec2,
+    pub color: Color,
 }
 
 impl HexTile {
-    pub fn new(size: f32, pos: Vec2) -> HexTile {
-        HexTile { size, pos }
+    pub fn new(size: f32, pos: Vec2, color: Color) -> HexTile {
+        HexTile { size, pos, color }
+    }
+
+    pub fn is_inside(&self, p: Vec2) -> bool {
+        let mut points = vec![];
+
+        for i in 0..6 {
+            points.push(
+                (self.size / 2.0 / (PI / 6.0).cos()
+                    * Vec2::new(
+                        (2.0 * PI / 6.0 * (i as f32) + 0.5 * PI).cos(),
+                        (2.0 * PI / 6.0 * (i as f32) + 0.5 * PI).sin(),
+                    ))
+                    + self.pos,
+            )
+        }
+        for i in 0..6 {
+            let prod = if i == 5 {
+                (points[i] - p)
+                    .extend(0.0)
+                    .cross((points[0] - p).extend(0.0))
+            } else {
+                (points[i] - p)
+                    .extend(0.0)
+                    .cross((points[i + 1] - p).extend(0.0))
+            };
+
+            if prod.z < 0.0 {
+                return false;
+            }
+        }
+
+        true
     }
 
     pub fn draw(&self, ctx: &mut Context) -> GameResult {
@@ -29,7 +62,7 @@ impl HexTile {
             )
         }
 
-        let inner = Mesh::new_polygon(ctx, DrawMode::fill(), &points, Color::BLACK)?;
+        let inner = Mesh::new_polygon(ctx, DrawMode::fill(), &points, self.color)?;
         let border = Mesh::new_polygon(ctx, DrawMode::stroke(2.0), &points, Color::WHITE)?;
 
         graphics::draw(ctx, &inner, (self.pos,))?;
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,59 @@
+use crate::event::MouseButton;
 use ggez::event;
 use ggez::graphics;
+use ggez::graphics::Color;
 use ggez::{Context, GameResult};
 use glam::*;
 use hex_sweeper::hex_tile::HexTile;
 use std::f32::consts::PI;
 
 struct MainState {
-    pos_x: f32,
+    grid: Vec<HexTile>,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
+        let mut grid = vec![];
+        for i in 0..10 {
+            for j in 0..10 {
+                grid.push(HexTile::new(
+                    50.0,
+                    Vec2::new(
+                        i as f32 * 50.0 + 25.0 * if j % 2 == 0 { 0.0 } else { 1.0 } + 25.0,
+                        j as f32 * (50.0 / 2.0 / (PI / 6.0).cos() + 50.0 / 2.0 * (PI / 6.0).tan()) + 25.0,
+                    ),
+                    Color::BLACK,
+                ));
+            }
+        }
+        let s = MainState { grid };
         Ok(s)
     }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
-        for i in 0..10 {
-            for j in 0..10 {
-                HexTile::new(
-                    50.0,
-                    Vec2::new(
-                        i as f32 * 50.0 + 50.0 + 25.0 * if j % 2 == 0 { 0.0 } else { 1.0 },
-                        j as f32 * (50.0 / 2.0 / (PI / 6.0).cos() + 50.0 / 2.0 * (PI / 6.0).tan()) + 50.0,
-                    ),
-                )
-                .draw(ctx)?;
-            }
+        for i in &self.grid {
+            i.draw(ctx)?;
         }
 
         graphics::present(ctx)?;
         Ok(())
+    }
+
+    fn mouse_button_up_event(&mut self, _ctx: &mut Context, _button: MouseButton, x: f32, y: f32) {
+        println!("{} {}", x, y);
+        for i in &mut self.grid {
+            if i.is_inside(Vec2::new(x, y)) {
+                i.color = Color::WHITE;
+            }
+        }
     }
 }
 
